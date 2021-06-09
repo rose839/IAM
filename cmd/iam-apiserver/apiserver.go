b145ba0aeb466f34bc27b37649b4e2aec1cc67ae
chen hao <617418745@qq.com>
2021-06-10 00:15:26 +0800

    apiserver: commit apiserver main func

---
+++ cmd/iam-apiserver/apiserver.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"runtime"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Getenv("GOMAXPROCS") == 0) {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+}
