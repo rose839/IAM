5e6fd8e37721f9e1d2b17e7ed235be362257490f
chen hao <617418745@qq.com>
2021-06-16 00:23:07 +0800

    feat: add general app definition

---
+++ pkg/app/app.go
@@ -0,0 +1,31 @@
+package app
+
+// App is the main structure of a cli app, it's recommanded
+// that an app be created with the app.NewApp func.
+type App struct {
+	basename string
+	name string
+	description string
+	runFunc RunFunc
+}
+
+// Option defines optional parameters for initializing the application
+// structure.
+type Option func(*App)
+
+// RunFunc defines the application's startup callback function.
+type RunFunc func(basename string) error
+
+// WithRunFunc is used to set the application startup callback function option.
+func WithRunFunc(run RunFunc) Option {
+	return func(app *App) {
+		app.runFunc = run
+	}
+}
+
+// WithDescription is used to set the description of the application.
+func WithDescription(desc string) Option {
+	return func(app *App) {
+		app.description = desc
+	}
+}
