39bedafa6a4f5b3f8295b13d529854a34b916eff
rose839 <617418745@qq.com>
2021-09-12 23:49:56 +0800

    refactor(cli command): add cli command attr

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -3,10 +3,13 @@ package app
 // App is the main structure of a cli app, it's recommanded
 // that an app be created with the app.NewApp func.
 type App struct {
-	basename string
-	name string
+	basename    string
+	name        string
 	description string
-	runFunc RunFunc
+	runFunc     RunFunc
+	slience     bool
+	noVersion   bool
+	noConfig    bool
 }
 
 // Option defines optional parameters for initializing the application
@@ -30,9 +33,32 @@ func WithDescription(desc string) Option {
 	}
 }
 
-func NewApp(name string, basename string, opts... Option) *App {
-	a := &App {
-		name: name,
+// WithSilence sets the application to silent mode, in which the program startup
+// information, configuration information, and version information are not
+// printed in the console.
+func WithSilence() Option {
+	return func(app *App) {
+		app.slience = true
+	}
+}
+
+// WithNoVersion set the application does not provide version flag.
+func WithNoVersion() Option {
+	return func(a *App) {
+		a.noVersion = true
+	}
+}
+
+// WithNoConfig set the application does not provide config flag.
+func WithNoConfig() Option {
+	return func(a *App) {
+		a.noConfig = true
+	}
+}
+
+func NewApp(name string, basename string, opts ...Option) *App {
+	a := &App{
+		name:     name,
 		basename: basename,
 	}
 
