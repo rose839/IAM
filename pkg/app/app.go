a846b16a67d88d45aa3bf1c23e4646266133ebb0
rose839 <617418745@qq.com>
2021-09-13 13:28:01 +0800

    feat(app cli frame): add app cli frame

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,15 +1,29 @@
 package app
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/marmotedu/log"
+	"github.com/spf13/cobra"
+)
+
+var progressMessage = color.GreenString("==>")
+
 // App is the main structure of a cli app, it's recommanded
 // that an app be created with the app.NewApp func.
 type App struct {
 	basename    string
-	name        string
-	description string
-	runFunc     RunFunc
+	name        string  // short description
+	description string  // long description
+	runFunc     RunFunc // user-defined main func
 	slience     bool
 	noVersion   bool
 	noConfig    bool
+	commands    []*Command           // sub command
+	args        cobra.PositionalArgs // validation func of positional arguments(arg is belong to command, not flag)
+	cmd         *cobra.Command       // root command
 }
 
 // Option defines optional parameters for initializing the application
@@ -56,6 +70,30 @@ func WithNoConfig() Option {
 	}
 }
 
+// WithValidArgs set the validation function to valid non-flag arguments.
+func WithValidArgs(args cobra.PositionalArgs) Option {
+	return func(a *App) {
+		a.args = args
+	}
+}
+
+// WithDefaultValidArgs set default validation function to valid non-flag arguments.
+func WithDefaultValidArgs() Option {
+	return func(a *App) {
+		a.args = func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+
+			return nil
+		}
+	}
+}
+
+// NewApp creates a new application instance based on the given application name,
+// base name, and other options.
 func NewApp(name string, basename string, opts ...Option) *App {
 	a := &App{
 		name:     name,
@@ -66,5 +104,58 @@ func NewApp(name string, basename string, opts ...Option) *App {
 		o(a)
 	}
 
+	a.buildCommand()
+
 	return a
+}
+
+func (a *App) buildCommand() {
+	cmd := cobra.Command{
+		Use:           a.basename,
+		Short:         a.name,
+		Long:          a.description,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		Args:          a.args,
+	}
+
+	cmd.SetOut(os.Stdout)
+	cmd.SetErr(os.Stderr)
+	cmd.Flags().SortFlags = true
+	initFlag(cmd.Flags())
+
+	if a.runFunc != nil {
+		cmd.RunE = a.runCommand
+	}
+
+	a.cmd = &cmd
+}
+
+// Run is used to launch the application.
+func (a *App) Run() {
+	if err := a.cmd.Execute(); err != nil {
+		fmt.Printf("%v %v\n", color.RedString("Error:"), err)
+		os.Exit(1)
+	}
+}
+
+// Command returns cobra command instance inside the application.
+func (a *App) Command() *cobra.Command {
+	return a.cmd
+}
+
+func (a *App) runCommand(cmd *cobra.Command, args []string) error {
+	printWorkingDir()
+	PrintFlag(cmd.Flags())
+
+	if a.runFunc != nil {
+		return a.runFunc(a.basename)
+	}
+
+	return nil
+}
+
+func printWorkingDir() {
+	wd, _ := os.Getwd()
+	log.Infof("%v WorkingDir: %s", progressMessage, wd)
 }
---
+++ pkg/app/flag.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	goflag "flag"
+	"strings"
+
+	"github.com/rose839/log"
+	"github.com/spf13/pflag"
+)
+
+func initFlag(flags *pflag.FlagSet) {
+	flags.SetNormalizeFunc(WordSepNormalizeFunc)
+	flags.AddGoFlagSet(goflag.CommandLine)
+}
+
+// WordSepNormalizeFunc changes all flags that contain "_" separators.
+func WordSepNormalizeFunc(_ *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		return pflag.NormalizedName(strings.Replace(name, "_", "-", -1))
+	}
+	return pflag.NormalizedName(name)
+}
+
+func PrintFlag(flags *pflag.FlagSet) {
+	flags.VisitAll(func(flag *pflag.Flag) {
+		log.Debugf("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
+}
