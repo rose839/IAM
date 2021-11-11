74c9d42962be374dc1f981aa6df3b7fd66b22f18
rose839 <617418745@qq.com>
2021-11-11 19:14:31 +0800

    feat(pkg): add errors pkg

---
+++ pkg/errors/doc.go
@@ -0,0 +1,3 @@
+// Package errors is the secondary development based on "github.com/pkg/errors",
+// it is compatible with "github.com/pkg/errors", and business code is provided.
+package errors // import "github.com/rose839/IAM/pkg/errors"
