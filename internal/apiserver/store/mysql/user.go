9701ae7e38e908f9fd4d0438e87e1b0926353ec0
rose839 <617418745@qq.com>
2021-11-30 00:31:10 +0800

    feat(apiserver): add user store

--- internal/apiserver/store/mysql/user.go
+++ internal/apiserver/store/mysql/user.go
@@ -1,6 +1,14 @@
 package mysql
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	v1 "github.com/rose839/IAM/api/apiserver/v1"
+	metav1 "github.com/rose839/IAM/api/meta/v1"
+	"github.com/rose839/IAM/internal/pkg/code"
+	"github.com/rose839/IAM/pkg/errors"
+	"gorm.io/gorm"
+)
 
 type users struct {
 	db *gorm.DB
@@ -8,4 +16,52 @@ type users struct {
 
 func newUsers(ds *dataStore) *users {
 	return &users{db: ds.db}
+}
+
+// Create creates a new user account.
+func (u *users) Create(ctx context.Context, user *v1.User, opts metav1.CreateOptions) error {
+	return u.db.Create(&user).Error
+}
+
+// Update updates an user account information.
+func (u *users) Update(ctx context.Context, user *v1.User, opts metav1.UpdateOptions) error {
+	return u.db.Save(user).Error
+}
+
+// Delete deletes the user by the user identifier.
+func (u *users) Delete(ctx context.Context, username string, opts metav1.DeleteOptions) error {
+	if opts.Unscoped {
+		u.db = u.db.Unscoped()
+	}
+
+	err := u.db.Where("name = ?", username).Delete(&v1.User{}).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
+	return nil
+}
+
+// DeleteCollection batch deletes the users.
+func (u *users) DeleteCollection(ctx context.Context, usernames []string, opts metav1.DeleteOptions) error {
+	if opts.Unscoped {
+		u.db = u.db.Unscoped()
+	}
+
+	return u.db.Where("name in (?)", usernames).Delete(&v1.User{}).Error
+}
+
+// Get return an user by the user identifier.
+func (u *users) Get(ctx context.Context, username string, opts metav1.GetOptions) (*v1.User, error) {
+	user := &v1.User{}
+	err := u.db.Where("name = ?", username).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.WithCode(code.ErrUserNotFound, err.Error())
+		}
+
+		return nil, errors.WithCode(code.ErrDatabase, err.Error())
+	}
+
+	return user, nil
 }
