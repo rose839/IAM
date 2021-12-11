782287a3e4c85fa532595d94c4edc7ab921bc984
rose839 <617418745@qq.com>
2021-12-11 17:41:34 +0800

    feat(apiserver): complete policy controller

---
+++ internal/apiserver/controller/v1/policy/delete_collection.go
@@ -0,0 +1,19 @@
+package policy
+
+import (
+	"github.com/gin-gonic/gin"
+	metav1 "github.com/rose839/IAM/api/meta/v1"
+	"github.com/rose839/IAM/internal/pkg/middleware"
+	"github.com/rose839/IAM/pkg/core"
+)
+
+func (p *PolicyController) DeleteCollection(c *gin.Context) {
+	if err := p.srv.Policies().DeleteCollection(c, c.GetString(middleware.UsernameKey),
+		c.QueryArray("name"), metav1.DeleteOptions{}); err != nil {
+		core.WriteResponse(c, err, nil)
+
+		return
+	}
+
+	core.WriteResponse(c, nil, nil)
+}
